e5afd665f88e4221df1b9694c3f6eefa9b683e4d
Satyam Mishra <58325532+triden005@users.noreply.github.com>
2020-09-01 10:40:51 +0530

    Add files via upload

--- clientnew/src/App.js
+++ clientnew/src/App.js
@@ -1,30 +1,47 @@
 import React from 'react';
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import {Container} from "reactstrap"
+import {ThemeContext, themes} from './theme-context';
+import ThemedButton from './themed-button';
 
-import AppNavBar from "./components/AppNavBar"
-import ShopingList from "./components/ShoppingList";
-import ItemModal from "./components/ItemModal"
-
-import {Provider} from "react-redux";
-
-import store from "./store";
-
-function App() {
+// An intermediate component that uses the ThemedButton
+function Toolbar(props) {
   return (
-    <Provider store={store}>
-        <div className="">
-          <AppNavBar/>
-          <Container>
-            <ItemModal/>
-            <ShopingList/>
-          </Container>
-            
-        </div>
-    </Provider>
-    
+    <ThemedButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemedButton>
   );
 }
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.light,
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark
+            ? themes.light
+            : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    // The ThemedButton button inside the ThemeProvider    // uses the theme from state while the one outside uses    // the default dark theme    
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+          </ThemeContext.Provider>
+          <div>
+            {/* <ThemedButton /> */}
+          </div>
+      </div>
+    );
+  }
+}
+
 export default App;
+
